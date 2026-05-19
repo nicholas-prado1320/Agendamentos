012515195d77526ac48e0b080ac54537e45579da
nicholas prado <nicholas.ps1000@gmail.com>
2026-05-19 14:50:31 -0300

    feat: define application routes with home component and auth guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { HomeComponent } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: '**', redirectTo: 'home' },
+];
