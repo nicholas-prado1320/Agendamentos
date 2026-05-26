8925ec1c7216459a6e2b23c2e023927b4c20876b
nicholas prado <nicholas.ps1000@gmail.com>
2026-05-26 10:51:51 -0300

    feat: add routing for clientes, novo-cliente, agendamentos, novo-agendamento, servicos, and novo-servico components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,31 @@ export const routes: Routes = [
 
     { path: 'home', component: HomeComponent, canActivate: [authGuard] },
 
+    {
+        path: 'clientes',
+        loadComponent: () => import('./features/clientes/clientes').then(m => m.Clientes)
+    },
+    {
+        path: 'novo-cliente',
+        loadComponent: () => import('./features/novo-cliente/novo-cliente').then(m => m.NovoCliente)
+    },
+    {
+        path: 'agendamentos',
+        loadComponent: () => import('./features/agendamentos/agendamentos').then(m => m.Agendamentos)
+    },
+    {
+        path: 'novo-agendamento',
+        loadComponent: () => import('./features/novo-agendamento/novo-agendamento').then(m => m.NovoAgendamento)
+    },
+    {
+        path: 'servicos',
+        loadComponent: () => import('./features/servicos/servicos').then(m => m.Servicos)
+    },
+    {
+        path: 'novo-servico',
+        loadComponent: () => import('./features/novo-servico/novo-servico').then(m => m.NovoServico)
+    },
+
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: '**', redirectTo: 'home' },
 ];
