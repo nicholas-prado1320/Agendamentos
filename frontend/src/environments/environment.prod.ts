11e5007e05fd39da4ba3cc64d7945334556b553f
nicholas prado <nicholas.ps1000@gmail.com>
2026-06-08 16:34:53 -0300

    fix: add googleClientId to environment configurations and include Google Identity Services script in index.html

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   vapidPublicKey: 'BJ-XSNHJaeR0ceL6FbqnJm9YMohEyYLGfJqXxwZHfQoHVNyW9_Xn3KhsEJBVvsawhvt1Bj36otiMeFC601QKYFI',
+  googleClientId: '204850510168-malqapae44gago0a8g6h2686p21e86ed.apps.googleusercontent.com'
 };
